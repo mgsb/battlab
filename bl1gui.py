874c0ebe6987c303979250a0574ef534abecf865
Mark Grosen <mark@grosen.org>
2021-07-31 14:30:43 -0700

    Updated plotting for better scaling

--- bl1gui.py
+++ bl1gui.py
@@ -4,6 +4,8 @@ from PySimpleGUI.PySimpleGUI import SELECT_MODE_SINGLE
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib
+matplotlib.use('TkAgg')
 
 from battlab import BattLabOne
 
@@ -17,14 +19,17 @@ def draw_figure(canvas, figure, loc=(0, 0)):
 class App:
     def __init__(self):
         self.bl1 = BattLabOne()
-        self.data = [0] * 100
         self.samples = None
         self.to_ms = 100
+        self.block_size = 250
+        self.data = [0] * self.block_size
 
         self.fig, self.axes = plt.subplots()
-        self.axes.set_xlim(0, 100)
-        self.axes.set_ylim(0, 2.5)
-        self.x = [i for i in range(100)]
+        # self.axes.set_xlim(0, 100)
+        # self.axes.set_ylim(0, 2.5)
+        self.x = [i for i in range(self.block_size)]
+        # self.axes.autoscale(enable=True, axis="y")
+        # plt.autoscale(True)
         self.line, = self.axes.plot([], [])
         _, _, fw, fh = self.fig.bbox.bounds
 
@@ -38,7 +43,7 @@ class App:
                                                   key="voltage",
                                                   size=(8, 2),
                                                   select_mode=SELECT_MODE_SINGLE)],
-            [sg.Text("Current Range: "),
+            [sg.Text("Current Range:"),
              sg.Radio(text="High", group_id="crange", default=True, key="crange-high"),
              sg.Radio(text="Low", group_id="crange", key="crange-low")],
             [sg.Button('Sample'), sg.Button("Stop", disabled=True)],
@@ -47,29 +52,34 @@ class App:
         ]
 
     def dispatch_event(self, event, values):
-        self.DISPATCH[event](self, values)
+        return self.DISPATCH[event](self, values)
 
     def sample(self, values):
-        self.bl1.current_range = self.bl1.CurrentRange.HIGH
-        self.bl1.voltage = 1.2 # values["voltage"][0]
-        self.samples = self.bl1.sample()
         self.to_ms = 0
 
         self.window["Sample"].update(disabled=True)
         self.window["Stop"].update(disabled=False)
 
+        self.axes.set_xlim(0, self.block_size)
+        # self.axes.set_ylim(0, 2.5)
+        self.data = [0] * self.block_size
         plt.title("")
         plt.draw()
-        self.axes.set_xlim(0, 100)
-        self.axes.set_ylim(0, 2.5)
-        self.data = [0] * 100
+
+        self.bl1.current_range = self.bl1.CurrentRange.HIGH \
+            if values["crange-high"] else self.bl1.CurrentRange.LOW
+        self.bl1.voltage = values["voltage"][0]
+        self.samples = self.bl1.sample()
+
         self.animation.resume()
 
-    def stop(self, values):
+        return True
+
+    def stop(self, *_):
         self.samples = None
         self.to_ms = 100
         self.bl1.reset()
-        del self.data[0:100]
+        del self.data[0:self.block_size]
         results = ("{} samples\navg: {:.3f} max: {:.3f} " +
                    "min: {:.3f}").format(len(self.data),
                                          sum(self.data) / len(self.data),
@@ -85,32 +95,46 @@ class App:
         self.line.set_xdata([i for i in range(len(self.data))])
         self.axes.set_ylim(ymin=min(self.data), ymax=max(self.data))
         self.line.set_ydata(self.data)
-
         plt.title(results)
         plt.draw()
 
-    def timeout(self, values):
+        return True
+
+    def timeout(self, *_):
         if self.samples:
             self.data.append(next(self.samples))
+        return True
 
     def setup_plot(self, canvas):
         self.fig_blit = draw_figure(canvas, self.fig)
 
-        def animate(i):
+        def animate(*_):
             self.line.set_xdata(self.x)
-            self.line.set_ydata(self.data[-100:])
+            self.line.set_ydata(self.data[-self.block_size:])
+            # self.axes.autoscale(enable=True, axis="both")
+            # self.axes.relim()
+            # self.axes.autoscale_view()
+            self.axes.set_ylim(min(self.data[-self.block_size:]),
+                               max(self.data[-self.block_size:]) + 0.1)
             return self.line,
 
         self.animation = animation.FuncAnimation(self.fig, animate, interval=20,
-                                           blit=True)
+                                                 blit=True)
     def set_window(self, window):
         self.setup_plot(window["canvas"].TKCanvas)
         self.window = window
+        return True
+
+    def shutdown(self, *_):
+        self.bl1.voltage = 0
+        self.bl1.reset()
+        return False
 
     DISPATCH = {
         "Sample": sample,
         "Stop": stop,
-        "__TIMEOUT__": timeout
+        "__TIMEOUT__": timeout,
+        None: shutdown,
     }
 
 def main():
@@ -118,18 +142,14 @@ def main():
 
     app = App()
     window = sg.Window("BattLab-One", app.layout, finalize=True,
-                       force_toplevel=True, font=("Arial", 12))
+                       force_toplevel=True) #, font=("Arial", 12))
     app.set_window(window)
 
     while True:
         event, values = window.read(timeout=app.to_ms)
 
-        if event == sg.WIN_CLOSED:
+        if not app.dispatch_event(event, values):
             break
-
-        app.dispatch_event(event, values)
-
-    app.bl1.voltage = 0
 
     window.close()
 
